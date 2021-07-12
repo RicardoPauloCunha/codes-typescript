ed194a68e9bee6ed9c1d2a90996f6da91c2ea9a8
Ricardo Paulo <ricadopaulo-jiros@outlook.com>
2021-07-12 17:47:28 -0300

    feat: organizando arquivos

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,21 @@
 "use strict";
 // Define o input1 como o tipo input
 var input1 = document.getElementById("num1");
+
 // Define o input2 como o tipo input
 var input2 = document.getElementById("num2");
+
 // ! avisa que tem o button
 var button = document.getElementById("button");
+
 // Define os tipos dos parametros, como o tipo number
 function sum(a, b) {
     return a + b;
 }
+
 // Converte a string para number
 button.addEventListener("click", function () {
     console.log(sum(Number(input1.value), Number(input2.value)));
 });
+
 // * Se o TS não reclama então deixa como está
--- script/decorators.ts
+++ script/decorators.ts
@@ -1,68 +1,68 @@
 // ** Class Decorator: Default values da class
 
-// // Factory
-// function Logger(prefix: string){
-//     return (target) => {
-//         console.log(`${prefix} - ${target}`);
-//     }
-// }
+// Factory
+function Logger(prefix: string){
+    return (target: any) => {
+        console.log(`${prefix} - ${target}`);
+    }
+}
 
-// @Logger("awesome")
-// class Foo {
+@Logger("awesome")
+class Foo {
 
-// }
+}
 
-// function setAPIVersion(apiVersion: string){
-//     return (constructor) => {
-//         return class extends constructor {
-//             version = apiVersion
-//         }
-//     }
-// }
+function setAPIVersion(apiVersion: string){
+    return (constructor: any) => {
+        return class extends constructor {
+            version = apiVersion
+        }
+    }
+}
 
-// // Decorator - anotar a versão da API
-// @setAPIVersion('1.0.0')
-// class API {
+// Decorator - anotar a versão da API
+@setAPIVersion('1.0.0')
+class API {
 
-// }
+}
 
-// console.log(new API())
+console.log(new API());
 
 // ** Property Decorator: DataAnnotations
 
-// function minLength(length: number){
-//     return (target: any, key: string) => {
-//         let val = target[key];
+function minLength(length: number){
+    return (target: any, key: string) => {
+        let val = target[key];
 
-//         const getter = () => val;
+        const getter = () => val;
 
-//         const setter = (value: string) => {
-//             if (value.length < length)
-//                 console.log(`Error: você não pode criar ${key} com tamanho menor que ${length}`);
-//             else
-//                 val = value;
-//         }
+        const setter = (value: string) => {
+            if (value.length < length)
+                console.log(`Error: você não pode criar ${key} com tamanho menor que ${length}`);
+            else
+                val = value;
+        }
 
-//         Object.defineProperty(target, key, {
-//             get: getter,
-//             set: setter,
-//         })
-//     }
-// }
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter,
+        })
+    }
+}
 
-// class Movie {
-//     // Validação - se for menor que 5 - erro
-//     @minLength(5)
-//     title: string;
+class Movie {
+    // Validação - se for menor que 5 - erro
+    @minLength(5)
+    title: string;
 
-//     constructor(t: string)
-//     {
-//         this.title = t;
-//     }
-// }
+    constructor(t: string)
+    {
+        this.title = t;
+    }
+}
 
-// const movie = new Movie("Interstellar");
-// console.log(movie.title);
+const movie = new Movie("Interstellar");
+console.log(movie.title);
 
 // ** Method Decorater
 
